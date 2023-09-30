f7e7472b215365ec08c38cc18e603874edc41f49
JassKaur <121858089+Jass-TheAnalyst@users.noreply.github.com>
2023-09-30 19:31:47 +0530

    Solutions to Adhoc-Requests

---
+++ List of markets.sql
@@ -0,0 +1,10 @@
+SELECT count(*) FROM gdb023.fact_sales_monthly; -- 9,71,631
+-- 1. Provide the list of markets in which 
+-- customer "Atliq Exclusive" operates its
+-- business in the APAC region.
+SELECT distinct market FROM dim_customer;
+select 
+distinct market,
+customer from dim_customer
+where customer='Atliq Exclusive'
+and region='APAC';
